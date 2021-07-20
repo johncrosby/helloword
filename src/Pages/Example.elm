dcb59e2c8643c19e5e7ac4550f80d07da036958c
John Crosby <johncrosby36@gmail.com>
2021-07-20 12:39:49 +0100

    added example page

---
+++ src/Pages/Example.elm
@@ -0,0 +1,19 @@
+module Pages.Example exposing (page)
+
+import Gen.Params.Example exposing (Params)
+import Page exposing (Page)
+import Request
+import Shared
+import View exposing (View)
+
+
+page : Shared.Model -> Request.With Params -> Page
+page shared req =
+    Page.static
+        { view = view
+        }
+
+
+view : View msg
+view =
+    View.placeholder "Example"
